fb3f8d771e72eb94cbb3156d3c1d887ef00a7440
EivindSul <eivindsulen2002@gmail.com>
2023-07-31 17:55:03 +0200

    initial

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,31 @@
+-- Themes
+return {
+	"typicode/bg.nvim",
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+
+	{
+		"rose-pine/nvim",
+		name = "rose-pine",
+	},
+
+	"mhartington/oceanic-next",
+
+	{
+		"sainnhe/gruvbox-material",
+		init = function ()
+			vim.g.gruvbox_material_transparent_background = 1
+			if vim.g.neovide then
+				vim.g.gruvbox_material_background = "medium"
+				vim.g.gruvbox_material_transparent_background = 0
+			end
+		end,
+	},
+
+	"sainnhe/everforest",
+
+	"savq/melange-nvim"
+}
---
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,92 @@
+return {
+	{
+		"lervag/vimtex",
+		lazy = false,
+		init = function()
+			vim.g.vimtex_view_method = 'skim'
+			vim.g.vimtex_quickfix_enabled = 1
+			vim.g.vimtex_syntax_enabled = 1
+			vim.g.vimtex_quickfix_mode = 0
+		end,
+		config = function ()
+			local map = require("helpers.keys").map
+			map("n", "<leader>ti", "<plug>(vimtex-info)", "VimTeX info")
+			map("n", "<leader>tI", "<plug>(vimtex-info-full)", "VimTeX info full")
+			map("n", "<leader>tt", "<plug>(vimtex-toc-open)", "Show table of contents")
+			map("n", "<leader>tT", "<plug>(vimtex-toc-toggle)", "Toggle table of contents")
+			map("n", "<leader>tq", "<plug>(vimtex-log)", "Show log")
+			map("n", "<leader>tv", "<plug>(vimtex-view)", "Open pdf viewer")
+			map("n", "<leader>tr", "<plug>(vimtex-reverse-search)", "Reverse search")
+			map("n", "<leader>tl", "<plug>(vimtex-compile)", "Toggle continuous compiler")
+			map("n", "<leader>tk", "<plug>(vimtex-stop)", "Stop continuous compiler")
+			map("n", "<leader>tL", "<plug>(vimtex-compile-ss)", "Compile document")
+			map("n", "<leader>tK", "<plug>(vimtex-stop-all)", "Stop compilation for all files")
+			map("n", "<leader>te", "<plug>(vimtex-errors)", "Open error window")
+			map("n", "<leader>to", "<plug>(vimtex-compile-output)", "Open file where output is directed")
+			map("n", "<leader>tg", "<plug>(vimtex-status)", "Show compilation status")
+			map("n", "<leader>tG", "<plug>(vimtex-status-all)", "Show compilation status for all files")
+			map("n", "<leader>tc", "<plug>(vimtex-clean)", "Clean auxillary files")
+			map("n", "<leader>tC", "<plug>(vimtex-clean-full)", "Clean all output")
+			map("n", "<leader>tm", "<plug>(vimtex-imaps-list)", "Show insert-mode mappings")
+			map("n", "<leader>tx", "<plug>(vimtex-reload)", "Reload VimTeX")
+			map("n", "<leader>tX", "<plug>(vimtex-reload-state)", "Reload current buffer")
+			map("n", "<leader>ts", "<plug>(vimtex-toggle-main)", "Set file as main")
+			map("n", "<leader>ta", "<plug>(vimtex-context-menu)", "Show context menu on item below cursor")
+			-- map("nx", "<leader>tL", "<plug>(vimtex-compile-selected)", "Compile the selected part of the current file")
+			-- map("n", "dse", "<plug>(vimtex-env-delete)", "placeholder")
+			-- map("n", "dsc", "<plug>(vimtex-cmd-delete)", "placeholder")
+			-- map("n", "ds", "<plug>(vimtex-env-delete-math)", "placeholder")
+			-- map("n", "dsd", "<plug>(vimtex-delim-delete)", "placeholder")
+			-- map("n", "cse", "<plug>(vimtex-env-change)", "placeholder")
+			-- map("n", "csc", "<plug>(vimtex-cmd-change)", "placeholder")
+			-- map("n", "cs", "<plug>(vimtex-env-change-math)", "placeholder")
+			-- map("n", "csd", "<plug>(vimtex-delim-change-math)", "placeholder")
+			-- map("nx", "tsf", "<plug>(vimtex-cmd-toggle-frac)", "placeholder")
+			-- map("n", "tsc", "<plug>(vimtex-cmd-toggle-star)", "placeholder")
+			-- map("n", "tse", "<plug>(vimtex-env-toggle-star)", "placeholder")
+			-- map("n", "ts", "<plug>(vimtex-env-toggle-math)", "placeholder")
+			-- map("n", "<F6>", "<plug>(vimtex-env-surround-line)", "placeholder")
+			-- |<plug>(vimtex-env-surround-operator)|          "n"
+			-- <F6>             |<plug>(vimtex-env-surround-visual)|            "x"
+			-- map("nx", "tsd", "<plug>(vimtex-delim-toggle-modifier)", "placeholder")
+			-- map("nx", "tsD", "<plug>(vimtex-delim-toggle-modifier-reverse)", "placeholder")
+			-- <F7>             |<plug>(vimtex-cmd-create)|                     "nxi"
+			-- map("i", "]]", "<plug>(vimtex-delim-close)", "placeholder")
+			-- <F8>             |<plug>(vimtex-delim-add-modifiers)|            "n"
+			-- map("xo", "ac", "<plug>(vimtex-ac)", "placeholder")
+			-- map("xo", "ic", "<plug>(vimtex-ic)", "placeholder")
+			-- map("xo", "ad", "<plug>(vimtex-ad)", "placeholder")
+			-- map("xo", "id", "<plug>(vimtex-id)", "placeholder")
+			-- map("xo", "ae", "<plug>(vimtex-ae)", "placeholder")
+			-- map("xo", "ie", "<plug>(vimtex-ie)", "placeholder")
+			-- map("xo", "a", "<plug>(vimtex-a$)", "placeholder")
+			-- map("xo", "i", "<plug>(vimtex-i$)", "placeholder")
+			-- map("xo", "aP", "<plug>(vimtex-aP)", "placeholder")
+			-- map("xo", "iP", "<plug>(vimtex-iP)", "placeholder")
+			-- map("xo", "am", "<plug>(vimtex-am)", "placeholder")
+			-- map("xo", "im", "<plug>(vimtex-im)", "placeholder")
+			-- map("nxo", "%", "<plug>(vimtex-%)", "placeholder")
+			-- map("nxo", "]]", "<plug>(vimtex-]])", "placeholder")
+			-- map("nxo", "][", "<plug>(vimtex-][)", "placeholder")
+			-- map("nxo", "[]", "<plug>(vimtex-[])", "placeholder")
+			-- map("nxo", "[[", "<plug>(vimtex-[[)", "placeholder")
+			-- map("nxo", "]", "<plug>(vimtex-]m)", "placeholder")
+			-- map("nxo", "]", "<plug>(vimtex-]M)", "placeholder")
+			-- map("nxo", "[", "<plug>(vimtex-[m)", "placeholder")
+			-- map("nxo", "[", "<plug>(vimtex-[M)", "placeholder")
+			-- map("nxo", "]", "<plug>(vimtex-]n)", "placeholder")
+			-- map("nxo", "]", "<plug>(vimtex-]N)", "placeholder")
+			-- map("nxo", "[", "<plug>(vimtex-[n)", "placeholder")
+			-- map("nxo", "[", "<plug>(vimtex-[N)", "placeholder")
+			-- map("nxo", "]", "<plug>(vimtex-]r)", "placeholder")
+			-- map("nxo", "]", "<plug>(vimtex-]R)", "placeholder")
+			-- map("nxo", "[", "<plug>(vimtex-[r)", "placeholder")
+			-- map("nxo", "[", "<plug>(vimtex-[R)", "placeholder")
+			-- map("nxo", "]/", "<plug>(vimtex-]/", "placeholder")
+			-- map("nxo", "]*", "<plug>(vimtex-]star", "placeholder")
+			-- map("nxo", "[/", "<plug>(vimtex-[/", "placeholder")
+			-- map("nxo", "[*", "<plug>(vimtex-[star", "placeholder")
+			-- map("n", "K", "<plug>(vimtex-doc-package)", "placeholder")
+		end
+	},
+}
