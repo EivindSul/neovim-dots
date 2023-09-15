ee5377a19d70675bc188db66c79e46ef57f1f4ea
EivindSul <eivindsulen2002@gmail.com>
2023-09-15 12:43:00 +0200

    commented code in ftplugin/tex as to not create conflicts with original keybinds

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -4,21 +4,21 @@
 -- \\ with shift+enter
 -- \\ på slutten av linjen med ctrl+o og ctrl+O
 
-function map (mode, lhs, rhs, desc)
-	vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
-end
-
-function unmap (mode, lhs)
-	vim.keymap.del(mode, lhs)
-end
-
--- Add bold or italic text through Ctrl+b and Ctrl+i
-map("n", "<C-b>", "i\\textbf{}<esc>", "Insert bold text environment")
-map("n", "<C-i>", "i\\textit{}<esc>", "Insert italic text environment")
-
--- Add bold or italic environment around currently selected text. Does not work in visual line mode
-map("x", "<C-b>", "c\\textbf{}<esc>P", "Insert bold text environment")
-map("x", "<C-i>", "c\\textit{}<esc>P", "Insert italic text environment")
+-- function map (mode, lhs, rhs, desc)
+	-- vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+-- end
+-- 
+-- function unmap (mode, lhs)
+	-- vim.keymap.del(mode, lhs)
+-- end
+-- 
+-- -- Add bold or italic text through Ctrl+b and Ctrl+i
+-- map("n", "<C-b>", "i\\textbf{}<esc>", "Insert bold text environment")
+-- map("n", "<C-i>", "i\\textit{}<esc>", "Insert italic text environment")
+-- 
+-- -- Add bold or italic environment around currently selected text. Does not work in visual line mode
+-- map("x", "<C-b>", "c\\textbf{}<esc>P", "Insert bold text environment")
+-- map("x", "<C-i>", "c\\textit{}<esc>P", "Insert italic text environment")
 
 -- unmap("nv", "<tab>")
 
